20b78c236842f40597f092f7c720cf01cbde1d7f
ChubaOraka <ChubaOraka@users.noreply.github.com>
2021-01-12 03:12:21 -0500

    Taking ownership

--- src/inc/footer.js
+++ src/inc/footer.js
@@ -9,9 +9,10 @@ export class Footer extends React.Component {
     render() {
         return (
             <div className="copyright">
-                Designed and built by <br/>
-                <a href="https://github.com/volkansengul" target="_blank">@volkansengul</a>,
+                Designed and built by <a href="https://github.com/volkansengul" target="_blank">@volkansengul</a>,
                 <a href="https://github.com/kenangundogan" target="_blank">@kenangundogan</a>
+                <br/>
+                Redesigned, repurposed and rebuilt by <a href="https://github.com/ChubaOraka" target="_blank">@ChubaOraka</a>
             </div>
         )
     }
